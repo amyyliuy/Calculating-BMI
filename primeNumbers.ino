1312e5bad8590f59aa391134a39e60935b6f24e0
amyyliuy <97140422+amyyliuy@users.noreply.github.com>
2022-01-06 17:34:51 +0800

    Update primeNumbers.ino

--- primeNumbers.ino
+++ primeNumbers.ino
@@ -1,20 +1,19 @@
 /* Loops is useful when there is a need to generate a series of numbers in a sequence.
 Generate the 1st 30 prime number in the number system.
 
- Learn to seek for hint yourself (online)
 What's the 26th Prime number in your series? Does your answer match up with everyone else?
 
 */
 
-long i = 0;
-long j = 0;
+int i = 0;
+int j = 0;
 boolean prime;
 
 void setup() {
   Serial.begin(115200);
   delay(1000)
   Serial.println("Here are the first 30 prime numbers in the number system: ")
-  for ( i = 2; 
+  for (
     
 
 
