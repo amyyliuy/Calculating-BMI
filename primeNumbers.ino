14dd2cf64f924b9b451438fca8d41f7d2adeb53a
amyyliuy <97140422+amyyliuy@users.noreply.github.com>
2022-01-06 17:55:43 +0800

    Update primeNumbers.ino

--- primeNumbers.ino
+++ primeNumbers.ino
@@ -2,7 +2,6 @@
 Generate the 1st 30 prime number in the number system.
 
 What's the 26th Prime number in your series? Does your answer match up with everyone else?
-
 */
 
 int i = 0;
@@ -13,18 +12,17 @@ void setup() {
   Serial.begin(115200);
   delay(1000)
   Serial.println("Here are the first 30 prime numbers in the number system: ")
-  for (
-    
 
-
-    prime = true;
-
-    for (j = 3; j < Jmax; j+=2){
-      if (i%j == 0){
+  for (i = 2; i <=30; i++) {
+    for (j = 3; j <= 30; j+=2) {
+      if (i % j == 0) {
         prime = false;
         break;
       }
     }
+  } else {
+      prime = true;
+      Serial.println(prime);
 
   
 }
